21eb8f3c2aadb8cd719d18f008b177b2aef0ceac
Rodhis <rodrigonoliva@gmail.com>
2025-07-03 19:33:17 -0300

    added routes to api

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,13 @@
-import { Router } from "express";
-import { TaskController } from "./controllers/TaskController";
+import { Router } from 'express'
+import { TaskController } from './controllers/TaskController'
 
-const router = Router();
+const router = Router()
 const taskController = new TaskController()
 
+router.get('/api/tasks', taskController.index)
+router.post('/api/tasks', taskController.store)
+router.get('/api/tasks/:id', taskController.show)
+router.put('/api/tasks/:id', taskController.update)
+router.delete('/api/tasks/:id', taskController.delete)
+
+export { router }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use("api", router)
+
 
 const PORT = process.env.PORT || 3000;
 

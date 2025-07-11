c143b65a59e4d87de7fd4231bb79f2010bc01665
rodhis <rodrigonoliva@gmail.com>
2025-07-11 18:02:39 -0300

    removed ownerId. it is now generated based on the userId

--- src/schemas/validationSchemas.ts
+++ src/schemas/validationSchemas.ts
@@ -1,17 +1,15 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const taskCreateSchema = z.object({
     title: z.string().min(2).max(100),
     description: z.string().min(5).max(500),
-    status: z.enum(["to-do", "in-progress", "done"]),
-    priority: z.enum(["low", "medium", "high"]),
-    ownerId: z.number().int().positive()
-});
+    status: z.enum(['to-do', 'in-progress', 'done']),
+    priority: z.enum(['low', 'medium', 'high']),
+})
 
 export const taskUpdateSchema = z.object({
     title: z.string().min(2).max(100).optional(),
     description: z.string().min(5).max(500).optional(),
-    status: z.enum(["to-do", "in-progress", "done"]).optional(),
-    priority: z.enum(["low", "medium", "high"]).optional(),
-    ownerId: z.number().int().positive().optional()
+    status: z.enum(['to-do', 'in-progress', 'done']).optional(),
+    priority: z.enum(['low', 'medium', 'high']).optional(),
 })

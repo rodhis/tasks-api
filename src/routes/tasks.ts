7ec8ca592514c5e2445de130c3c275255feb5727
rodhis <rodrigonoliva@gmail.com>
2025-07-11 18:14:14 -0300

    removed "tasks" prefix on tasks routes

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -5,10 +5,10 @@ import { authenticate, authorizeTaskOwner } from '../middlewares/authMiddleware'
 const tasksRouter = Router()
 const taskController = new TaskController()
 
-tasksRouter.get('/tasks', authenticate, taskController.index)
-tasksRouter.get('/tasks/:id', authenticate, authorizeTaskOwner, taskController.show)
-tasksRouter.post('/tasks', authenticate, taskController.create)
-tasksRouter.put('/tasks/:id', authenticate, authorizeTaskOwner, taskController.update)
-tasksRouter.delete('/tasks/:id', authenticate, authorizeTaskOwner, taskController.delete)
+tasksRouter.get('/', authenticate, taskController.index)
+tasksRouter.get('/:id', authenticate, authorizeTaskOwner, taskController.show)
+tasksRouter.post('/', authenticate, taskController.create)
+tasksRouter.put('/:id', authenticate, authorizeTaskOwner, taskController.update)
+tasksRouter.delete('/:id', authenticate, authorizeTaskOwner, taskController.delete)
 
 export default tasksRouter

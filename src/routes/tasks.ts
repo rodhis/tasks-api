c9b633e93fc157a94ead3aec54aad9b0f7863f50
rodhis <rodrigonoliva@gmail.com>
2025-07-11 18:04:06 -0300

    protected all routes accordingly on tasks router

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -5,8 +5,8 @@ import { authenticate, authorizeTaskOwner } from '../middlewares/authMiddleware'
 const tasksRouter = Router()
 const taskController = new TaskController()
 
-tasksRouter.get('/tasks', taskController.index)
-tasksRouter.get('/tasks/:id', taskController.show)
+tasksRouter.get('/tasks', authenticate, taskController.index)
+tasksRouter.get('/tasks/:id', authenticate, authorizeTaskOwner, taskController.show)
 tasksRouter.post('/tasks', authenticate, taskController.create)
 tasksRouter.put('/tasks/:id', authenticate, authorizeTaskOwner, taskController.update)
 tasksRouter.delete('/tasks/:id', authenticate, authorizeTaskOwner, taskController.delete)

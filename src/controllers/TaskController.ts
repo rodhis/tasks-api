c673a354b53025253e4adfdeaa620bffeec493bc
rodhis <rodrigonoliva@gmail.com>
2025-07-11 18:03:21 -0300

    redone tasks controller to get ownerId based on userId

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -2,15 +2,16 @@ import { Handler, Request, Response } from 'express'
 import { Task } from '../models/Task'
 import { HttpError } from '../errors/HttpError'
 import { taskCreateSchema, taskUpdateSchema } from '../schemas/validationSchemas'
+import { AuthRequest } from '../middlewares/authMiddleware'
 
 export class TaskController {
-    //GET: /tasks
-    index: Handler = (req: Request, res: Response) => {
-        const tasks = Task.findAll()
-        res.json(tasks)
+    index: Handler = (req: AuthRequest, res: Response) => {
+        const allTasks = Task.findAll()
+
+        const userTasks = allTasks.filter((task) => task.ownerId === req.user!.id)
+        res.json(userTasks)
     }
 
-    //GET: /tasks/:id
     show: Handler = (req: Request, res: Response) => {
         const { id } = req.params
         const task = Task.findById(+id)
@@ -18,23 +19,24 @@ export class TaskController {
         res.json(task)
     }
 
-    //POST: /tasks
-    create = (req: Request, res: Response) => {
+    create: Handler = (req: AuthRequest, res: Response) => {
         const parsedBody = taskCreateSchema.parse(req.body)
-        const newTask = Task.create(parsedBody)
+
+        const newTask = Task.create({
+            ...parsedBody,
+            ownerId: req.user!.id,
+        })
         res.status(201).json(newTask)
     }
 
-    //PUT: /tasks/:id
     update: Handler = (req: Request, res: Response) => {
         const { id } = req.params
         const parsedBody = taskUpdateSchema.parse(req.body)
         const updatedTask = Task.update(+id, parsedBody)
         if (!updatedTask) throw new HttpError(404, 'Task not found')
-        res.status(201).json(updatedTask)
+        res.status(200).json(updatedTask)
     }
 
-    //DELETE: /tasks/:id
     delete: Handler = (req: Request, res: Response) => {
         const { id } = req.params
         const deletedTask = Task.delete(+id)
